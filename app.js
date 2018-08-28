6fec3c82bb0e334aca66169033d9461aa5758730
Neko0w0 <fran.18.neko@gmail.com>
2018-08-28 18:05:55 -0400

    first comentary

---
+++ app.js
@@ -0,0 +1,19 @@
+/*
+var sumaDeDosNumeros =2 + 2;
+//asdjlkasj
+var texto = prompt();
+document.write (texto);*/
+
+var primerBotom =document.getElementById("botom1");
+var segundoBotom =document.getElementById("botom2");
+
+//funciones
+function saludo(){
+    console.log ("soy el botom 1")
+};
+function saludo2(){
+document.write("soy un botom 2")
+
+};
+
+
