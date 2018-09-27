043c3c4d58ef05ff2a618edab2b41aad481ada3b
Neko0w0 <fran.18.neko@gmail.com>
2018-09-27 16:26:55 -0400

    clase 2 mas cosas

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var sumaDeDosNumeros =2 + 2;
 //asdjlkasj
 var texto = prompt();
-document.write (texto);*/
+document.write (texto);
 
 var primerBotom =document.getElementById("botom1");
 var segundoBotom =document.getElementById("botom2");
@@ -14,6 +14,17 @@ function saludo(){
 function saludo2(){
 document.write("soy un botom 2")
 
-};
+};*/
 
+$("document").ready(function(){
+    var buttom = $("#botom1");
+    var buttomDos = $("#botom2");
 
+    buttom.click(function(){
+        console.log("hola soy un botom")
+    });
+    buttomDos.click(function(){
+//$("a").attr("href","https://www.google.cl/") redireccion
+buttom.addClass("purple darken-4")
+    });
+});
